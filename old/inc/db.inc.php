71710d1afb6ff64c7caf7ecfd27eda18b32e215e
Siim Aarmaa <51194757+siimaarmaa@users.noreply.github.com>
2022-11-06 21:25:31 +0200

    Version Change

---
+++ old/inc/db.inc.php
@@ -0,0 +1,13 @@
+<?php
+/* Andemaasi andmed */
+$db_host		= 'localhost';
+$db_user		= 'smaapoco_antslag';
+$db_pass		= 'OQsTE-T8;cSh';
+$db_database	= 'smaapoco_antslagymnaasium'; 
+ 
+/* Seda alt poolt enam ei näppi */
+ 
+$db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+?>
